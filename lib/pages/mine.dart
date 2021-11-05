6e7ef5df8291907020463fd0042106b398465e2e
pangwenji <799953277@qq.com>
2021-11-05 09:09:04 +0800

    2021-11-5

--- lib/pages/mine.dart
+++ lib/pages/mine.dart
@@ -17,6 +17,7 @@ class Mine extends StatelessWidget {
             },
             body:SingleChildScrollView(
                       child: Container(
+                        padding: EdgeInsets.all(10),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
@@ -26,7 +27,57 @@ class Mine extends StatelessWidget {
                                      Container(
                                        child: Image.asset('images/avatar.png'),
                                      ),
-                                     Container()
+                                     Container(
+                                       padding: EdgeInsets.only(left: 10),
+                                       width: 290,
+                                       child: Column(
+                                         crossAxisAlignment: CrossAxisAlignment.start,
+                                         children: [
+                                           Text('用户名： pangwenji<普通会员>',
+                                              style: TextStyle(
+                                                  fontSize: 15
+                                              ),
+                                           ),
+                                           Text('昵称：pangwenji',
+                                               style: TextStyle(
+                                                  fontSize: 12
+                                              ),
+                                           )
+                                         ],
+                                       ),
+                                     ),
+                                     Container(
+                                       child: Icon(Icons.arrow_forward_ios,size: 10,),
+                                     )
+                                  ],
+                                ),
+                              ),
+                              //第二行
+                              Container(
+                                margin: EdgeInsets.only(top: 5),
+                                height: 60,
+                                width: MediaQuery.of(context).size.width,
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      color: Colors.grey.shade300,
+                                      width: 6),
+                                      top: BorderSide(
+                                      color: Colors.grey.shade300,
+                                      width: 6),
+                                      )
+                                ),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      child: Image.asset('images/location.png',width: 18,height: 18,),
+                                    ),
+                                    Container(
+                                      child: Text('我的集运专属地址（国内仓/神州仓）'),
+                                    ),
+                                    Container(
+                                       child: Icon(Icons.arrow_forward_ios,size: 10,),
+                                    )
                                   ],
                                 ),
                               )
